6708fd3e1e2686c06f5413cf17b16e5fb7904188
Perl72 <perlgonzales72@gmail.com>
2025-06-26 15:09:28 -0600

    modifying inputs and adding init for libs

---
+++ lib/python_utils/__init__.py
@@ -0,0 +1,8 @@
+from .downloader5 import *           # includes your downloading logic
+from .make_clips import *            # includes clip-making routines
+from .tasks_lib import *             # core task logic
+from .transcription_utils import *   # transcription support
+from .video_utils import *           # logging, video functions
+from .watermarker2 import *          # watermarking logic
+from .whisper_utils import *         # Whisper-related functions
+
